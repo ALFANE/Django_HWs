6c20b5f006f5f652d80d6351f73e923097add878
Evhenii Podolian <alfan2620@gmail.com>
2024-02-23 01:13:13 +0200

    first commit on branch dz_13 Выполнено задание: Страница с зачетками
    Создать страницу, которая будет отображать все зачетки. Также краткую информацию о студенте к которому эта зачетка принадлежит
    Каждая зачетка должна быть в виде ссылки чтобы можно было перейти на страницу редактирования зачетки, чтобы удалить зачетку у студента
    Страница с предметами
    
    Создать страницу для отображения всех предметов и списка студентов к каждому предмету
    Каждый предмет должен быть в виде ссылки чтобы можно было перейти на страницу редактирования и удалить студента из предмета или добавить нового студента к предмету
    Также эта страница должна иметь возможность редактировать название предмета
    Страница с учителями
    
    Создать страницу для отображения всех учителей и списка студентов привязанных к учителям
    Каждый учитель должен отображаться в виде ссылки чтобы можно было перейти на страницу редактирования учителя. Удалять студентов от учителя или добавлять студента к учителю
    Также эта страница должна иметь возможность редактировать информацию об учителе
    Страница со студентами
    
    Подправить страницу со всеми студентами, страница должна отображать информацию об студенте, его зачетке, предмете и учителях
    Добавит сверху списка поля формы в которых можно будет вводить фильтр по которому будет фильтроваться список студентов
    Поле-фильтр по учителю (ввожу имя учителя и выводятся студенты только конкретного учителя)
    Поле-фильтр по предмету (ввожу название предмета и выводятся студенты с конкретных передметом)
    Поле-фильтр по зачетке (ввожу номер зачетки и выводится студент с данной зачеткой)
    Все страницы должны быть сделанные через Bootstrap

--- home/test.py
+++ home/test.py
@@ -1,12 +1,2 @@
 from faker import Faker
-from gender_guesser.detector import Detector
 
-faker = Faker()
-# faker.add_provider("faker.providers.job")
-
-a = []
-for i in range(10):
-    a.append(faker.name())
-
-# Пример использования
-print(a)
